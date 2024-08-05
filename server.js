806acb0c5d69fe8efc70b27d8fd8146e5a491cf5
hansika1120 <137263599+hansika1120@users.noreply.github.com>
2024-08-05 22:06:35 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
-const express = require('express');
-const app = express();
+var http = require('http');
 
-const port = 3000;
-app.get('/',(req,res)=>{
-    res.send('Hello World!');
-})
-
-app.listen(port,()=>console.log(`app listen at port ${port}`));
+//create a server object:
+http.createServer(function (req, res) {
+  res.write('A Monk in Cloud'); //write a response to the client
+  res.end(); //end the response
+}).listen(80); //the server object listens on port 80
